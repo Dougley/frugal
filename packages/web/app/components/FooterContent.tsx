7874001381ca24287362c19b540acc32158693a8
Remco <hey@dougley.com>
2023-01-04 20:16:52 +0000

    socials

--- packages/web/app/components/FooterContent.tsx
+++ packages/web/app/components/FooterContent.tsx
@@ -1,14 +1,17 @@
 import type { ReactElement } from 'react';
-import { BsTwitter } from 'react-icons/bs';
-import { GiCrocJaws } from 'react-icons/gi';
+import { BsGithub, BsTwitter, BsMastodon } from 'react-icons/bs';
+import { FaDog } from 'react-icons/fa';
+import { SiGithubsponsors } from 'react-icons/si';
 import { Link } from '@remix-run/react';
 
 function FooterContent(): ReactElement {
   return (
     <div className="footer p-10 bg-neutral text-neutral-content">
       <div>
-        <GiCrocJaws size={50} />
-        <p>&copy; Dougley - {new Date().getFullYear()}</p>
+        <FaDog size={50} />
+        <Link to="//dougley.com" className="link link-hover">
+          <p>&copy; Dougley - {new Date().getFullYear()}</p>
+        </Link>
         <p className="text-xs text-slate-700">
           <Link
             to="//github.com/Dougley/frugal/tree/main/packages/web"
@@ -24,6 +27,19 @@ function FooterContent(): ReactElement {
         <div className="grid grid-flow-col gap-4">
           <Link to="//twitter.com/dougley" target="_blank" rel="noreferrer">
             <BsTwitter size={24} />
+          </Link>
+          <Link to="//mas.to/@doug" target="_blank" rel="noreferrer">
+            <BsMastodon size={24} />
+          </Link>
+          <Link to="//github.com/Dougley" target="_blank" rel="noreferrer">
+            <BsGithub size={24} />
+          </Link>
+          <Link
+            to="//github.com/sponsors/Dougley"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiGithubsponsors size={24} />
           </Link>
         </div>
       </div>
